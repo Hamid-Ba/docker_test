914366a47e021e3d8303f95fe9fe1d5be3994f19
hamidba <balalzadehhamid79@gmail.com>
2023-09-21 13:31:34 +0330

    add account urls

--- config/urls.py
+++ config/urls.py
@@ -13,9 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
 from django.conf import settings
 from django.contrib import admin
+from django.urls import path, include
 from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
@@ -28,6 +28,7 @@ urlpatterns = [
         SpectacularSwaggerView.as_view(url_name="api-schema"),
         name="api-docs",
     ),
+    path("api/account/", include("account.urls")),
 ]
 
 if settings.DEBUG:
